404489111ff29284cb26cba7069544bb9dc00fb5
Álvaro Tipian <alvarito.tipian@gmail.com>
2025-08-10 17:37:35 -0500

    Diseño base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TAREA_semana3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
